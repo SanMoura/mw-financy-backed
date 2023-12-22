3b5092098d8af732c899d02826318cec2edc344a
Marlus <santiago.marlus@gmail.com>
2023-12-22 19:38:29 -0300

    add auth jwt

---
+++ src/app.controller.ts
@@ -0,0 +1,18 @@
+import { Controller, Get, UseGuards } from '@nestjs/common';
+import { AppService } from './app.service';
+import { AuthGuard } from './auth/auth.guard';
+@Controller('/api')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+
+  // @Get()
+  // getHello(): string {
+  //   return this.appService.getHello();
+  // }
+
+  @UseGuards(AuthGuard)
+  @Get('/auth')
+  marlus(): string {
+    return this.appService.marlus();
+  }
+}
